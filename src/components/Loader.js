741fbd2a03e73570bfdc00b94ce11df4d5f86c92
raj shringi <raj24shringi@gmail.com>
2024-05-03 00:37:43 +0530

    add message in loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,13 @@
 function Loader() {
   return (
-    <div className="text-center my-4">
-      <div className="animate-spin w-8 h-8 border-4 rounded-full inline-block  border-t-transparent border-indigo-400"></div>
+    <div>
+      <div className="text-center my-4">
+        <div className="animate-spin w-8 h-8 border-4 rounded-full inline-block  border-t-transparent border-indigo-400"></div>
+      </div>
+      <h2 className="text-center text-lg text-bold">
+        Initializing service. This may take up to a minute. Thank you for your
+        patience.
+      </h2>
     </div>
   );
 }
